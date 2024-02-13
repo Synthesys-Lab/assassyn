c6f28a72d523764a94a4bea96c27e8f3814766f6
Jian Weng <jian.weng465@gmail.com>
2024-02-13 14:53:56 +0300

    a better frontend sense

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,10 @@
 pub mod builder;
 
+pub use builder::context;
 pub use builder::module::Module;
-pub use builder::data::Data;
-pub use builder::ctx::Context;
+pub use builder::data;
+pub use builder::context::Reference;
+pub use builder::data::DataType;
+pub use builder::arith;
+pub use builder::arith::Arithmetic;
 
