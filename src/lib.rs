71daa68568ed90b765679653d91f6a908b78841c
Jian Weng <jian.weng465@gmail.com>
2024-03-19 23:41:09 +0300

    TODO: a better parser

--- src/lib.rs
+++ src/lib.rs
@@ -1,185 +1,153 @@
-use proc_macro2::{Delimiter, TokenTree};
+use proc_macro::TokenStream;
 use quote::quote;
-use syn::spanned::Spanned;
+use syn::parse::Parse;
+use syn::parse_macro_input;
+use syn::{braced, bracketed};
 
-fn expect_ident(
-  prefix: &str,
-  pp: &mut std::iter::Peekable<proc_macro2::token_stream::IntoIter>,
-  allow_none: bool,
-) -> Result<Option<proc_macro2::Ident>, syn::Error> {
-  let tok = pp.next();
-  match tok {
-    Some(TokenTree::Ident(ident)) => Ok(ident.into()),
-    Some(other) => Err(syn::Error::new(
-      other.span(),
-      format!(
-        "{} Expect an identifier, but got {}",
-        prefix,
-        other.to_string()
-      ),
-    )),
-    None => {
-      if allow_none {
-        Ok(None)
-      } else {
-        Err(syn::Error::new(
-          proc_macro2::Span::call_site(),
-          format!("{} Expect an identifier, but got end of stream", prefix),
-        ))
+struct TypeParser {
+  ty: TokenStream,
+}
+
+impl Parse for TypeParser {
+  fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+    let id = input.parse::<syn::Ident>()?;
+    match id.to_string().as_str() {
+      "int" | "uint" => {
+        let _ = input
+          .parse::<syn::Token![<]>()
+          .map_err(|e| syn::Error::new(e.span(), "Expected <"))?;
+        let bits = input.parse::<syn::LitInt>()?;
+        let _ = input
+          .parse::<syn::Token![>]>()
+          .map_err(|e| syn::Error::new(e.span(), "Expected >"))?;
+        Ok(TypeParser {
+          ty: quote! {eir::frontend::DataType::#id(#bits)}.into(),
+        })
       }
+      _ => Err(syn::Error::new(
+        id.span(),
+        format!("Unsupported type: {}", id.to_string()),
+      )),
     }
   }
 }
 
-fn expect_punct_alone(
-  pp: &mut std::iter::Peekable<proc_macro2::token_stream::IntoIter>,
-  ch: char,
-  allow_none: bool,
-) -> Result<Option<proc_macro2::Punct>, syn::Error> {
-  let tok = pp.next();
-  match &tok {
-    Some(TokenTree::Punct(punct)) => {
-      if punct.as_char() == ch {
-        Ok(punct.clone().into())
-      } else {
-        Err(syn::Error::new(
-          tok.span(),
-          format!("Expect '{}', but got '{}'", ch, punct),
-        ))
+struct ArgumentParser {
+  ids: Vec<syn::Ident>,
+  tys: Vec<TokenStream>,
+}
+
+impl Parse for ArgumentParser {
+  fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+    let content;
+    let _ = bracketed!(content in input);
+    let mut ids = Vec::new();
+    let mut tys = Vec::new();
+    while !content.is_empty() {
+      let id = content
+        .parse::<syn::Ident>()
+        .map_err(|e| syn::Error::new(e.span(), "Expected a port id"))?;
+      ids.push(id);
+      let _ = content
+        .parse::<syn::Token![:]>()
+        .map_err(|e| syn::Error::new(e.span(), "Expected : to specify the type of the port"))?;
+      let ty = content.parse::<TypeParser>()?;
+      tys.push(ty.ty);
+      if content.is_empty() {
+        break;
       }
+      content
+        .parse::<syn::Token![,]>()
+        .map_err(|e| syn::Error::new(e.span(), "Each port should be separated by a comma"))?;
     }
-    None => {
-      if allow_none {
-        Ok(None)
-      } else {
-        Err(syn::Error::new(
-          proc_macro2::Span::call_site(),
-          format!("Expect '{}', but got end of stream", ch),
-        ))
-      }
-    }
-    _ => Err(syn::Error::new(
-      tok.span(),
-      format!("Expect '{}', but got {:?}", ch, tok),
-    )),
+    Ok(ArgumentParser { ids, tys })
   }
 }
 
-/// Parse a type.
-///
-/// Supported types: int<bits>, uint<bits>
-///
-/// TODO(@were): Support module callback type: module((types,)*)
-fn parse_type(
-  pp: &mut std::iter::Peekable<proc_macro2::token_stream::IntoIter>,
-) -> proc_macro2::TokenStream {
-  match expect_ident("[Parse Type]", pp, false) {
-    Ok(Some(raw_id)) => {
-      let ty_id = raw_id.to_string();
-      if ty_id.eq("int") || ty_id.eq("uint") {
-        expect_punct_alone(pp, '<', false).unwrap();
-        let bits = pp.next().unwrap();
-        expect_punct_alone(pp, '>', false).unwrap();
-        return quote! {eir::frontend::DataType::#raw_id(#bits)}.into();
-      } else {
-        return syn::Error::new(raw_id.span(), format!("Unsupported type: {}", ty_id))
-          .to_compile_error()
-          .into();
+struct ExternalInterfParser {
+  ids: Vec<syn::Ident>,
+}
+
+impl Parse for ExternalInterfParser {
+  fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+    let content;
+    let _ = bracketed!(content in input);
+    let mut ids = Vec::new();
+    while !content.is_empty() {
+      let id = input
+        .parse::<syn::Ident>()
+        .map_err(|e| syn::Error::new(e.span(), "Expected external interface id"))?;
+      ids.push(id);
+      if content.is_empty() {
+        break;
       }
+      input.parse::<syn::Token![,]>().map_err(|e| {
+        syn::Error::new(
+          e.span(),
+          "Each external interface id should be separated by a comma",
+        )
+      })?;
     }
-    Err(e) => {
-      return e.to_compile_error().into();
-    }
-    Ok(None) => unreachable!(),
+    Ok(ExternalInterfParser { ids })
   }
 }
 
-fn parse_module(input: proc_macro2::TokenStream) -> Result<proc_macro2::TokenStream, syn::Error> {
-  let mut pp = input.into_iter().peekable();
+struct ModuleParser {
+  module_name: syn::Ident,
+  builder_name: syn::Ident,
+  ports: ArgumentParser,
+  ext_interf: ExternalInterfParser,
+  body: BodyParser,
+}
 
-  let (module_name, builder_name) = {
-    let tok = expect_ident("[Module Name]", &mut pp, false)?.unwrap();
+enum Instruction {
+  Assign(syn::Ident, syn::Expr),
+}
+
+struct BodyParser {
+  stmts: Vec<Instruction>,
+}
+
+impl Parse for BodyParser {
+  fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+    let content;
+    let _ = braced!(content in input);
+    let mut stmts = Vec::new();
+    while !content.is_empty() {
+      let master = content.parse::<syn::Ident>()?;
+      // a = <expr>
+      if content.peek(syn::Token![=]) {
+        content.parse::<syn::Token![=]>()?;
+        let expr = content.parse::<syn::Expr>()?;
+        content.parse::<syn::Token![;]>()?;
+        stmts.push(Instruction::Assign(master, expr));
+      }
+    }
+    Ok(BodyParser { stmts })
+  }
+}
+
+impl Parse for ModuleParser {
+  fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+    let tok = input
+      .parse::<syn::Ident>()
+      .map_err(|e| syn::Error::new(e.span(), "Expected module name"))?;
     let module_name = tok.clone();
     let builder_name = syn::Ident::new(&format!("{}_builder", module_name.to_string()), tok.span());
-    (module_name, builder_name)
-  };
+    let ports = input.parse::<ArgumentParser>()?;
+    let ext_interf = input.parse::<ExternalInterfParser>()?;
+    let body = input.parse::<BodyParser>()?;
 
-  // parse ports
-  let (port_ids, port_infos, port_fifo_peeks) = {
-    let raw_args = pp.next();
-    let span = raw_args.span();
-    let mut port_infos = proc_macro2::TokenStream::new();
-    let mut port_ids = proc_macro2::TokenStream::new();
-    let mut port_fifo_peeks = proc_macro2::TokenStream::new();
-    let mut idx: usize = 0;
-    match raw_args {
-      Some(TokenTree::Group(args)) => {
-        assert_eq!(args.delimiter(), Delimiter::Bracket);
-        let mut iter = args.stream().into_iter().peekable();
-        while let Some(id) = expect_ident("[Arguments]", &mut iter, true)? {
-          expect_punct_alone(&mut iter, ':', false)?;
-          let ty = parse_type(&mut iter);
-          let pi: proc_macro2::TokenStream =
-            quote! { eir::frontend::PortInfo::new(stringify!(#id), #ty), };
-          port_infos.extend(pi);
-          let fifo_peek = quote! { let #id = module.get_input(#idx).unwrap().clone(); };
-          port_fifo_peeks.extend(fifo_peek);
-          port_ids.extend(quote! { #id, });
-          idx += 1;
-          expect_punct_alone(&mut iter, ',', true)?;
-        }
-      }
-      _ => {
-        return Err(syn::Error::new(
-          span,
-          format!("Expect a group of arguments, but got {:?}", raw_args),
-        ))
-      }
-    };
-    port_ids = quote! { (#port_ids) };
-    (port_ids, port_infos, port_fifo_peeks)
-  };
+    let res = Ok(ModuleParser {
+      module_name,
+      builder_name,
+      ports,
+      ext_interf,
+      body,
+    });
 
-  let ext_interf = {
-    let raw = pp.next();
-    let span = raw.span();
-    match raw {
-      Some(TokenTree::Group(args)) => {
-        assert_eq!(args.delimiter(), Delimiter::Bracket);
-        let mut ext_args = proc_macro2::TokenStream::new();
-        let mut iter = args.stream().into_iter().peekable();
-        while let Some(tok) = expect_ident("[Parse Ext Interf]", &mut iter, true)? {
-          ext_args.extend(quote! { #tok: eir::frontend::BaseNode, });
-          if let None = expect_punct_alone(&mut iter, ',', true)? {
-            break;
-          }
-        }
-        ext_args
-      }
-      _ => {
-        return Err(syn::Error::new(
-          span,
-          format!(
-            "Expect a group of external interfaces in [...], but got {:?}",
-            raw
-          ),
-        ))
-      }
-    }
-  };
-
-  Ok(quote!(
-    fn #builder_name (sys: &mut eir::frontend::SysBuilder, #ext_interf) -> eir::frontend::BaseNode {
-      let module = sys.create_module(stringify!(#module_name), vec![#port_infos]);
-      let #port_ids = {
-        let module = module.as_ref::<eir::frontend::Module>(&sys).unwrap();
-        #port_fifo_peeks
-        #port_ids
-      };
-      module
-    }
-  )
-  .into())
+    res
+  }
 }
 
 /// Parse a module builder macro.
@@ -188,9 +156,63 @@ fn parse_module(input: proc_macro2::TokenStream) -> Result<proc_macro2::TokenStr
 /// }
 #[proc_macro]
 pub fn module_builder(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-  let tt2 = proc_macro2::TokenStream::from(input);
-  match parse_module(tt2) {
-    Ok(tt) => tt.into(),
-    Err(e) => e.to_compile_error().into(),
-  }
+  let parsed_module = parse_macro_input!(input as ModuleParser);
+
+  eprintln!("module_name: {:?} parsed!", parsed_module.module_name);
+
+  let module_name = parsed_module.module_name;
+  let builder_name = parsed_module.builder_name;
+
+  // codegen ports
+  let (port_ids, port_decls, port_peeks): (
+    proc_macro2::TokenStream,
+    proc_macro2::TokenStream,
+    proc_macro2::TokenStream,
+  ) = {
+    let ports = &parsed_module.ports;
+    let mut port_ids = TokenStream::new();
+    let mut port_decls = TokenStream::new();
+    let mut port_peeks = TokenStream::new();
+    for (i, elem) in ports.ids.iter().enumerate() {
+      port_ids.extend::<TokenStream>(quote! { #elem, }.into());
+      port_peeks.extend::<TokenStream>(
+        quote! { let #elem = module.get_input(#i).unwrap().clone(); }.into(),
+      );
+    }
+    for elem in ports.ids.iter().zip(ports.tys.iter()) {
+      let id = elem.0;
+      let ty = proc_macro2::TokenStream::from(elem.1.clone());
+      port_decls
+        .extend::<TokenStream>(quote! {eir::frontend::PortInfo::new(stringify!(#id), #ty),}.into());
+    }
+    (port_ids.into(), port_decls.into(), port_peeks.into())
+  };
+
+  // codegen external interfaces
+  let ext_interf: proc_macro2::TokenStream = {
+    let ext_interf = &parsed_module.ext_interf;
+    let mut res = TokenStream::new();
+    for elem in ext_interf.ids.iter() {
+      res.extend::<TokenStream>(quote! { #elem, }.into());
+    }
+    res.into()
+  };
+
+  eprintln!("codegen is done!");
+
+  let res = quote! {
+    fn #builder_name (sys: &mut eir::frontend::SysBuilder, #ext_interf) -> eir::frontend::BaseNode {
+      let module = sys.create_module(stringify!(#module_name), vec![#port_decls]);
+      let ( #port_ids ) = {
+        let module = module.as_ref::<eir::frontend::Module>(&sys).unwrap();
+        #port_peeks
+        ( #port_ids )
+      };
+      module
+    }
+  };
+
+  eprintln!("{}", res);
+
+  res.into()
 }
--- tests/module.rs
+++ tests/module.rs
@@ -1,21 +1,14 @@
 use eda4eda::module_builder;
-use eir;
 
 #[test]
 fn foo() {
-  module_builder!(driver[][] {
-    cnt = array(int<32>, 1);
-    v = cnt[0];
-    v = v.add(1);
-    cnt[0] = v;
-  });
+  // module_builder!(driver[][] {});
 
   module_builder!(sqr[a:int<32>][] {
     a = a.pop();
-    bind func { a: a };
-    async sqr(a);
     b = a.mul(a);
   });
-  let mut sys = eir::frontend::SysBuilder::new("a");
-  sqr_builder(&mut sys);
+
+  // let mut sys = eir::frontend::SysBuilder::new("a");
+  // sqr_builder(&mut sys);
 }
