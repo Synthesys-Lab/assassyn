b8833035ef6472346c91a28ea7d93dec8c26be08
Jian Weng <jian.weng465@gmail.com>
2024-04-28 23:06:24 +0300

    o0 implemented

--- eir/src/xform/mod.rs
+++ eir/src/xform/mod.rs
@@ -4,8 +4,14 @@ mod callback;
 mod rewrite_wait_until;
 mod spin_trigger;
 
-pub fn basic(sys: &mut SysBuilder) {
+pub struct Config {
+  pub rewrite_wait_until: bool,
+}
+
+pub fn basic(sys: &mut SysBuilder, config: &Config) {
   callback::rewrite_fifos(sys);
   spin_trigger::rewrite_spin_triggers(sys);
-  rewrite_wait_until::rewrite_wait_until(sys);
+  if config.rewrite_wait_until {
+    rewrite_wait_until::rewrite_wait_until(sys);
+  }
 }
