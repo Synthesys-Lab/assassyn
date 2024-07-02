1040578fa8d22226b8a25fdf828da9e8679ed54f
Ruijie Gao <wuliJerry927@gmail.com>
2024-07-02 09:14:19 +0800

    support equal

--- python/assassyn/value.py
+++ python/assassyn/value.py
@@ -65,6 +65,11 @@ class Value:
         return BinaryOp(BinaryOp.IGE, self, other)
 
     @ir_builder(node_type='expr')
+    def __eq__(self, other):
+        from .expr import BinaryOp
+        return BinaryOp(BinaryOp.EQ, self, other)
+
+    @ir_builder(node_type='expr')
     def __invert__(self):
         from .expr import UnaryOp
         return UnaryOp(UnaryOp.FLIP, self)
