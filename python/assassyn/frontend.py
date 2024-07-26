8d4940aa8c0848fd443b82b8be0477482abb697e
Jian Weng <jian.weng465@gmail.com>
2024-07-26 15:43:35 +0300

    wip, remove !!!!

--- python/assassyn/frontend.py
+++ python/assassyn/frontend.py
@@ -5,7 +5,8 @@ from .array import RegArray, Array
 from .dtype import DType, Int, UInt, Float, Bits
 from .builder import SysBuilder, ir_builder, Singleton
 from .expr import Expr, log
-from .module import Module, Port, Memory
+from .module import Module, Port, Memory, Downstream
 from .value import Optional
 from .block import Condition, Cycle
 from . import module
+from .module import downstream
--- python/assassyn/module/downstream.py
+++ python/assassyn/module/downstream.py
@@ -1,7 +1,43 @@
 '''Downstream class is a special module that is combinational across multiple different
 chronological modules.'''
 
-from .base import ModuleBase
+from decorator import decorator
+
+from .base import ModuleBase, name_ports_of_module
+from ..value import Optional
+
+def constructor(func, *args, **kwargs):
+    '''Constructor decorator for the Downstream class.'''
+    builder = Singleton.builder
+    module_self = args[0]
+    builder.insert_point['module'].append(module_self)
+    func(*args, **kwargs)
+    name_ports_of_module(module_self, Optional)
+
+def combinatorial(func, *args, **kwargs):
+    '''Combinatorial decorator for the Downstream class.'''
+    return func(*args, **kwargs)
+
+@decorator
+#pylint: disable=keyword-arg-before-vararg
+def combinational(
+        func,
+        *args,
+        **kwargs):
+    '''A decorator for marking a function as combinational logic description.'''
+    module_self = args[0]
+    assert isinstance(module_self, Downstream)
+    Singleton.builder.cur_module = module_self
+    Singleton.builder.builder_func = func
+    module_self.body = Block(Block.MODULE_ROOT)
+    with module_self.body:
+        res = func(*args, **kwargs)
+    Singleton.builder.cleanup_symtab()
+    Singleton.builder.cur_module = None
+    return res
 
 class Downstream(ModuleBase):
     '''Downstream class implementation.'''
+
+    def __init__(self):
+        super().__init__()
