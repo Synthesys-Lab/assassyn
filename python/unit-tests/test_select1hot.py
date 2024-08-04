165c82bb7b2a277a022be38d0e4b4285e0d3d5e9
Ruijie Gao <wuliJerry927@gmail.com>
2024-08-04 19:27:06 +0800

    test_select1hot with crappy test case and no verilator support

--- python/unit-tests/test_select1hot.py
+++ python/unit-tests/test_select1hot.py
@@ -9,30 +9,16 @@ class Driver(Module):
     @module.constructor
     def __init__(self):
         super().__init__(disable_arbiter_rewrite=True)
-        self.cond = Port(Int(5))
         
     @module.combinational
     def build(self):
+        cond = RegArray(Int(5), 1, initializer=[1])
         values = RegArray(Int(32), 5, initializer = [1, 2, 4, 8, 16])
 
-        gt = self.cond
+        gt = cond[0]
         mux = gt.select1hot(values[0], values[1], values[2], values[3], values[4])
 
-        log("onehot select {:b}: {}", gt, mux)
-
-class Testbench(Module):
-    
-    @module.constructor
-    def __init__(self):
-        super().__init__()
-
-    @module.combinational
-    def build(self, driver: Module):
-        for i in range(1, 201):
-            with Cycle(i):
-                cond = 1 << (i % 5)
-                driver.async_called(cond = Int(5)(cond))
-
+        log("onehot select {:b} : {}", gt, mux)
 
 def check(raw: str):
     for i in raw.splitlines():
@@ -47,15 +33,16 @@ def test_select1hot():
         driver = Driver()
         driver.build()
 
-        testbench = Testbench()
-        testbench.build(driver)
-        
+    #TODO: verilator simulation for Select1Hot
 
-    # print(sys)
-    simulator_path = elaborate(sys, verilog=None)
+    # simulator_path, verilator_path = elaborate(sys, verilog='verilator', idle_threshold=5, sim_threshold=5)
+    simulator_path = elaborate(sys, verilog=None, idle_threshold=5, sim_threshold=5)
 
     raw = utils.run_simulator(simulator_path)
-    # check(raw)
+    check(raw)
+
+    # raw = utils.run_verilator(verilator_path)
+    # print(raw)
 
 if __name__ == '__main__':
     test_select1hot()
