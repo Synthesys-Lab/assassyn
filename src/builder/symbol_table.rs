0302655c04e74cf7547ee94523a69176a9d0f6fd
Jian Weng <jian.weng465@gmail.com>
2024-08-12 11:43:51 +0300

    unify symbol tables

--- src/builder/symbol_table.rs
+++ src/builder/symbol_table.rs
@@ -1,11 +1,16 @@
 use std::collections::HashMap;
 
+use crate::ir::node::BaseNode;
+
 pub(crate) struct SymbolTable {
   unique_ids: HashMap<String, u32>,
+  symbols: HashMap<String, BaseNode>,
 }
 
 impl SymbolTable {
-  pub(crate) fn identifier(&mut self, id: &str) -> String {
+
+  /// The helper function to find a unique identifier for the given identifier.
+  fn identifier(&mut self, id: &str) -> String {
     // If the identifier is already in the symbol table, we append a number to it.
     if let Some(x) = self.unique_ids.get_mut(id) {
       // Append a number after.
@@ -20,9 +25,31 @@ impl SymbolTable {
     id.into()
   }
 
+  /// Insert the given node into the symbol table.
+  pub(crate) fn insert(&mut self, id: &str, node: BaseNode) -> String {
+    let id = self.identifier(id);
+    self.symbols.insert(id.clone(), node);
+    return id;
+  }
+
+  /// Get the node from the symbol table.
+  pub(crate) fn get(&self, id: &str) -> Option<&BaseNode> {
+    self.symbols.get(id)
+  }
+
+  /// Erase the given node from the symbol table.
+  pub(crate) fn remove(&mut self, id: &str) -> Option<BaseNode> {
+    self.symbols.remove(id)
+  }
+
+  pub(crate) fn iter(&self) -> impl Iterator<Item = (&String, &BaseNode)> {
+    self.symbols.iter()
+  }
+
   pub(crate) fn new() -> Self {
     SymbolTable {
       unique_ids: HashMap::new(),
+      symbols: HashMap::new(),
     }
   }
 }
