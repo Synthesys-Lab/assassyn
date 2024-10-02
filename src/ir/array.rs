805946e7a0de441c55332b2896f7162ab071823b
Jian Weng <jian.weng465@gmail.com>
2024-10-02 10:32:06 +0300

    move memoryparam to array instead of module

--- src/ir/array.rs
+++ src/ir/array.rs
@@ -4,10 +4,67 @@ use crate::builder::SysBuilder;
 
 use super::{node::BaseNode, DataType, Typed};
 
+use std::fmt::Display;
+use std::ops::RangeInclusive;
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct MemoryParams {
+  pub array: BaseNode,
+  pub width: usize,
+  pub depth: usize,
+  pub lat: RangeInclusive<usize>,
+  pub init_file: Option<String>,
+}
+
+impl Default for MemoryParams {
+  fn default() -> Self {
+    Self {
+      array: BaseNode::unknown(),
+      width: 0,
+      depth: 0,
+      lat: 0..=0,
+      init_file: None,
+    }
+  }
+}
+
+impl MemoryParams {
+  pub fn new(
+    array: BaseNode,
+    width: usize,
+    depth: usize,
+    lat: RangeInclusive<usize>,
+    init_file: Option<String>,
+  ) -> Self {
+    Self {
+      array,
+      width,
+      depth,
+      lat,
+      init_file,
+    }
+  }
+}
+
+impl Display for MemoryParams {
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    write!(
+      f,
+      "width: {} depth: {} lat: [{:?}], file: {}",
+      self.width,
+      self.depth,
+      self.lat,
+      self.init_file.clone().map_or("None".to_string(), |x| x)
+    )
+  }
+}
+
+
+
 #[derive(Clone, PartialEq, Eq, Hash, Debug)]
 pub enum ArrayAttr {
   FullyPartitioned,
-  MemoryParams,
+  MemoryParams(MemoryParams),
 }
 
 pub struct Array {
