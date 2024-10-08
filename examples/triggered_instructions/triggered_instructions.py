e21d04e029bd8a70f5583ecf14c98b3912887bbd
RANDOLPH <li_ze_cheng@yeah.net>
2024-10-08 16:54:31 +0800

    PE init.

--- examples/triggered_instructions/triggered_instructions.py
+++ examples/triggered_instructions/triggered_instructions.py
@@ -2,3 +2,28 @@ import assassyn
 from assassyn.frontend import *
 from assassyn import backend
 from assassyn import utils
+
+#  # PE Array (4 + ?) x (4 + ?)
+
+#  [Pusher] [Compute PE]  [Compute PE]  [Compute PE]  [Compute PE]  
+#  [Pusher] [Compute PE]  [Compute PE]  [Compute PE]  [Compute PE]  
+#  [Pusher] [Compute PE]  [Compute PE]  [Compute PE]  [Compute PE]  
+#  [Pusher] [Compute PE]  [Compute PE]  [Compute PE]  [Compute PE]  
+
+class ComputePE(Module):
+
+    @module.constructor
+    def __init__(self):
+        super().__init__(disable_arbiter_rewrite=True)
+        self.input = Port(Int(32))
+        self.output = Port(Int(32))
+
+    @module.combinational
+    def build(self, input: Bind, output: Bind):
+        acc = RegArray(Int(32), 1)
+        val = acc[0]
+        mul = (self.west * self.north)
+        c = mul[0:31].bitcast(Int(32))
+        mac = val + c
+        log("Mac value: {} * {} + {} = {}", self.west, self.north, val, mac)
+        acc[0] = mac
