3b098566b8c9e764e74a4dea5b152009b45a1848
Jian Weng <jian.weng465@gmail.com>
2024-10-10 10:09:52 +0300

    move memory parameter attribute back to module attr

--- src/ir/module/attrs.rs
+++ src/ir/module/attrs.rs
@@ -1,4 +1,67 @@
-use super::Module;
+use std::ops::RangeInclusive;
+
+use crate::builder::SysBuilder;
+
+use super::{BaseNode, Module};
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct MemoryParams {
+  pub width: usize,
+  pub depth: usize,
+  pub lat: RangeInclusive<usize>,
+  pub init_file: Option<String>,
+  // Pins for memory module.
+  pub array: BaseNode,
+  pub re: BaseNode,
+  pub we: BaseNode,
+  pub addr: BaseNode,
+  pub wdata: BaseNode,
+}
+
+impl MemoryParams {
+  pub fn new(
+    width: usize,
+    depth: usize,
+    lat: RangeInclusive<usize>,
+    init_file: Option<String>,
+    array: BaseNode,
+    re: BaseNode,
+    we: BaseNode,
+    addr: BaseNode,
+    wdata: BaseNode,
+  ) -> Self {
+    Self {
+      width,
+      depth,
+      lat,
+      init_file,
+      array,
+      re,
+      we,
+      addr,
+      wdata,
+    }
+  }
+
+  pub fn is_sram(&self) -> bool {
+    return self.lat.start().eq(&1) && self.lat.end().eq(&1);
+  }
+
+  pub fn to_string(&self, sys: &SysBuilder) -> String {
+    format!(
+      "width: {}, depth: {}, lat: [{:?}], file: {}, array: {} (.we({}), .re({}), .addr({}), .wdata({}))",
+      self.width,
+      self.depth,
+      self.lat,
+      self.init_file.clone().map_or("None".to_string(), |x| x),
+      self.array.to_string(sys),
+      self.we.to_string(sys),
+      self.re.to_string(sys),
+      self.addr.to_string(sys),
+      self.wdata.to_string(sys),
+    )
+  }
+}
 
 macro_rules! define_attrs {
   ( $($attrs: ident $( ( $vty:ty ) )? ),* $(,)* ) => {
@@ -36,4 +99,6 @@ define_attrs!(
   Systolic,
   // This module is a downstream module, which is combinationally connected to the upstream module.
   Downstream,
+  // This module is a memory downstream module.
+  MemoryParams(MemoryParams),
 );
