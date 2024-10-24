4b81e3dad16afe1378b1d2bac17785061fc0768b
WanningZhang <nkzwning@outlook.com>
2024-10-24 11:11:46 +0800

    Built `SRAM and `user`, but `bitmask` and `rdata` do not match.

--- examples/memory_engine/v3.py
+++ examples/memory_engine/v3.py
@@ -5,20 +5,19 @@ from assassyn import utils
 import time
 import random
 
-current_seed = int(time.time())
+# current_seed = int(time.time())
 current_seed = 1000
 
 cachesize = 8
-bitmask = 0b10101010
 
 num_rows = 50
 num_columns = 30
 stride = 30
 
-random.seed(current_seed)
-num1 = random.randint(0, num_rows * num_columns)
-num2 = random.randint(0, num_rows * num_columns)
-start, end = sorted([num1, num2]) # Will get [start, end)
+# random.seed(current_seed)
+# num1 = random.randint(0, num_rows * num_columns)
+# num2 = random.randint(0, num_rows * num_columns)
+# start, end = sorted([num1, num2]) # Will get [start, end)
 
 start = 5
 end = 33
@@ -50,7 +49,10 @@ class MemUser(Module):
             offest = cachesize - i - 1
             data_masked = bitmask[offest:offest].select(rdata[offest*32:offest*32+31], Bits(32)(0))
             data_joint = data_masked if data_joint is None else data_joint.concat(data_masked)
-
+            
+            lineno = ((rdata[0:31].bitcast(Int(32)) + Int(32)(1)) >> Int(32)(3)) - Int(32)(1)
+        
+        log("\t\tGET:\t\tbitmask={:b}\tlineno={}\trdata={:x}", bitmask, lineno, rdata)
         log("Cacheline:\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}",
             data_joint[224:255],
             data_joint[192:223],
@@ -73,7 +75,15 @@ class Driver(Module):
         init = initialization[0]
         
         cnt_i = RegArray(Int(32), 1)
-        cnt_j = RegArray(Int(32), 1)
+        cnt_j = RegArray(Int(32), 1)        
+                
+        i = cnt_i[0]
+        j = cnt_j[0]
+
+        addr = (i * Int(32)(stride))[0:31].bitcast(Int(32)) + j
+        row_end = (i * Int(32)(stride))[0:31].bitcast(Int(32)) + Int(32)(stride)
+        shift = cachesize.bit_length() - 1
+        lineno = Bits(shift)(0).concat(addr[shift:8]).bitcast(Int(9))
         
         # Initialization.
         with Condition(~init):
@@ -81,35 +91,34 @@ class Driver(Module):
             cnt_i[0] = Int(32)(init_i)
             cnt_j[0] = Int(32)(init_j)
             
-            log("start:{} end:{} i:{} j:{}", Int(32)(start), Int(32)(end), Int(32)(init_i), Int(32)(init_j))
+            # log("******************************************    start:{} end:{} i:{} j:{}", Int(32)(start), Int(32)(end), Int(32)(init_i), Int(32)(init_j))
         
         # i and j have already been initialized.
-        with Condition(init):
-        
-            i = cnt_i[0]
-            j = cnt_j[0]
-
-            addr = (i * Int(32)(stride))[0:31].bitcast(Int(32)) + j
-            row_end = (i * Int(32)(stride))[0:31].bitcast(Int(32)) + Int(32)(stride)
-            shift = cachesize.bit_length() - 1
-            lineno = Bits(shift)(0).concat(addr[shift:8]).bitcast(Int(9))
+        with Condition(init): 
+            
             line_end = ((lineno +Int(9)(1))*Int(32)(cachesize))[0:31].bitcast(Int(32))
             offset = Bits(cachesize-shift)(0).concat(addr[0:shift-1]).bitcast(Bits(cachesize))
             reserve = Bits(cachesize)(2 ** cachesize - 1) >> offset
 
-            log("i={}\tj={}\taddr={}\trow_end={}\tlineno={}\tline_end={}\toffest={}\treserve={:b}", i, j, addr, row_end, lineno, line_end, offset, reserve)
-            
-            # sram = SRAM(width, 512, init_file)
-            # sram.build(Int(1)(0), Int(1)(1), lineno, Bits(width)(0), user)
+            sram = SRAM(width, 512, init_file)
+            sram.build(Int(1)(0), Int(1)(1), lineno, Bits(width)(0), user)            
             
             sentinel = (Int(32)(end) <= row_end).select(Int(32)(end), row_end)
             next = (Int(32)(end) <= row_end).select(Int(1)(0), Int(1)(1))
             
-            with Condition(line_end >= sentinel):
-                discard = (UInt(cachesize)(1) << ((line_end - sentinel).bitcast(UInt(32)))) - UInt(cachesize)(1)
-                bitmask = reserve ^ discard
-                log("get end: reserve={:b} discard={:b} bitmask={:b}", reserve, discard, bitmask)
-                
+            counter = (line_end >= sentinel).select((line_end - sentinel).bitcast(UInt(32)), UInt(32)(0))
+            discard = (UInt(cachesize)(1) << counter) - UInt(cachesize)(1)
+            
+            bitmask = (reserve ^ discard).bitcast(Bits(cachesize))            
+                        
+            log("\t\tCALL: bitmask={:b}\tlineno={}", bitmask, lineno)
+            
+            # log("___________________________i={}\tj={}\taddr={}\trow_end={}\tlineno={}\tline_end={}\toffest={}\tsentinel={}\treserve={:b} discard={:b} bitmask={:b}", i, j, addr, row_end, lineno, line_end, offset, sentinel, reserve, discard, bitmask)
+            
+            user.bind(mask=bitmask)
+            sram.bound.async_called()
+            
+            with Condition(line_end >= sentinel):                
                 # Read will go to next row.
                 with Condition(next):
                     cnt_i[0] = i + Int(32)(1)
@@ -119,24 +128,7 @@ class Driver(Module):
                     initialization[0] = Int(1)(0)
                 
             with Condition(line_end < sentinel):
-                bitmask = reserve
-                log("full read 1: bitmask is {:b}", bitmask)
                 cnt_j[0] = j + Int(32)(cachesize) - (Bits(32-cachesize)(0).concat(offset)).bitcast(Int(32))
-            
-
-        # raddr = v[0:8].bitcast(Int(9))
-        
-        # shift = Int(9)(cachesize.bit_length())
-        # addr_access = raddr >> shift
-        
-        # mask = Bits(cachesize)(bitmask)
-        # user.bind(mask=mask)
-        
-        # self.cnt[0] = plused
-        # sram = SRAM(width, 512, init_file)
-        # sram.build(we, re, addr_access, v.bitcast(Bits(width)), user)
-        # with Condition(re):
-        #     sram.bound.async_called()
 
 def check(raw):
     for line in raw.splitlines():
