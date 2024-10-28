527694ab5b22e5286462d3abda864a80df4cec58
H2Oxi <2562189344@qq.com>
2024-10-28 10:03:12 +0300

    fixed cpu using sp and add arth right shift

--- src/backend/verilog/elaborate.rs
+++ src/backend/verilog/elaborate.rs
@@ -1220,16 +1220,26 @@ module memory_blackbox_{a} #(
     let mut is_pop = None;
 
     let body = match expr.get_opcode() {
-      Opcode::Binary { .. } => {
-        let bin = expr.as_sub::<instructions::Binary>().unwrap();
-        format!(
-          "{} {} {}",
-          dump_arith_ref(self.sys, &bin.a()),
-          bin.get_opcode(),
-          dump_arith_ref(self.sys, &bin.b())
-        )
-      }
 
+        Opcode::Binary { binop } => {
+          let dtype = expr.dtype();
+          let bin = expr.as_sub::<instructions::Binary>().unwrap();
+          format!(
+            "{} {} {}",
+            dump_ref!(self.sys, &bin.a()),
+            if matches!(binop, subcode::Binary::Shr) {
+              if dtype.is_signed() {
+                ">>>".into()
+              } else {
+                ">>".into()
+              }
+            } else {
+              binop.to_string()
+            },
+            dump_ref!(self.sys, &bin.b())
+          )
+        }
+  
       Opcode::Unary { ref uop } => {
         let dump = match uop {
           subcode::Unary::Flip => "~",
