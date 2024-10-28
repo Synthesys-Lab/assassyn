df17b40f575114340a454b4cfd6e4cfa8db3d1d8
Jian Weng <jian.weng@kaust.edu.sa>
2024-10-28 17:11:54 +0000

    wip

--- examples/minor-cpu/workloads/gold.sh
+++ examples/minor-cpu/workloads/gold.sh
@@ -1,2 +1,12 @@
+for i in 3 5; do
 
-cat $1 | grep "raw:" | awk '{ i=3; if ($i ~ /raw:/) print $(i+1) }'
+trace=$(cat $1 | grep "raw:" | awk "{ i=$i; if (\$i ~ /raw:/) print \$(i+1) }")
+
+if [ "$trace" != "" ]; then
+  cat $1 | grep "raw:" | awk "{ i=$i; if (\$i ~ /raw:/) print \$(i+1) }" > a.trace
+fi
+
+done
+
+cat $2 | awk '{ print substr($5, 2, length($5) - 2) }' > a.gold
+
