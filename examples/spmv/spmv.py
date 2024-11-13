1805ad5afaeabc3f2c8fefe6b7ac73160feacaf1
H2Oxi <2562189344@qq.com>
2024-11-13 13:53:25 +0300

    update data gen

--- examples/spmv/spmv.py
+++ examples/spmv/spmv.py
@@ -11,8 +11,11 @@ from assassyn.frontend import *
 from assassyn.backend import *
 from assassyn import utils
 
-I_MAX = 5
-J_MAX = 8
+N_SIZE = 5
+L_SIZE = 8
+I_MAX = N_SIZE - 1
+J_MAX = L_SIZE - 1
+M_SIZE = N_SIZE * L_SIZE
 
 class SRAM_USER:
     IDLE = Bits(3)(0)
@@ -21,6 +24,7 @@ class SRAM_USER:
     M3 =   Bits(3)(3)
     OUT =  Bits(3)(4)
     JUMP = Bits(3)(5)
+    OUT_WAIT = Bits(3)(6)
 
 
 class SRAM_Master(Module):
@@ -30,7 +34,7 @@ class SRAM_Master(Module):
         ) 
         
     @module.combinational
-    def build(self):
+    def build(self, i: Array, j: Array):
         user_state = RegArray(Bits(3), 1)
         Start = self.pop_all_ports(False)
         SRAM_Master_flag = RegArray(Bits(1), 1)
@@ -44,32 +48,23 @@ class SRAM_Master(Module):
                 user_state[0] = SRAM_USER.M3
 
             with Condition(user_state[0] == SRAM_USER.M3):
-                user_state[0] = SRAM_USER.JUMP
+                user_state[0] = (j[0]==Int(32)(J_MAX)).select(  SRAM_USER.OUT, SRAM_USER.JUMP)
 
         with Condition(user_state[0] == SRAM_USER.IDLE):
             with Condition(Start == Bits(1)(1)):
                 user_state[0] = SRAM_USER.M1
 
+        with Condition(user_state[0] == SRAM_USER.OUT):
+            user_state[0] = SRAM_USER.JUMP
+
         with Condition(user_state[0] == SRAM_USER.JUMP):
             user_state[0] = SRAM_USER.IDLE
 
+        p = Int(32)(0)
+        p = (i[0][0:15].bitcast(Int(16)) * Int(16)(J_MAX+1)).bitcast(Int(32)) + j[0]
+        log("p: {} = i_{}, j_{}", p, i[0], j[0])
+
         return SRAM_Master_flag
-
-class Loop_user(Module):
-
-    def __init__(self):
-        ports={
-            
-        }
-        super().__init__(
-            ports=ports ,
-        )
-
-    @module.combinational
-    def build(self, i: Array, j: Array):
-        p = RegArray(Int(32), 1)
-        p[0] = (i[0][0:15].bitcast(Int(16)) * Int(16)(J_MAX+1)).bitcast(Int(32)) + j[0]
-        log("p: {} = i_{}, j_{}", p[0], i[0], j[0])
 
 class External_loop(Module):
     def __init__(self):
@@ -78,9 +73,9 @@ class External_loop(Module):
         )
 
     @module.combinational
-    def build(self,sram_master: SRAM_Master):
+    def build(self,sram_master: SRAM_Master,i:Array):
         In_full_flag = self.pop_all_ports(False)
-        i = RegArray(Int(32), 1)
+        
         full_flag = RegArray(Bits(1), 1)
 
 
@@ -90,9 +85,10 @@ class External_loop(Module):
             full_flag[0] = (i[0] == Int(32)(I_MAX)).bitcast(Bits(1))
             i[0] = con.select((i[0].bitcast(Int(32)) + Int(32)(1)) , Int(32)(0))
         
+        log("outterloop----------In_full_flag: {} | i: {} ", In_full_flag,i[0])
         sram_master.async_called(Start = ~full_flag[0])
 
-        return i
+        
 
 class Internal_loop(Module):
     def __init__(self):
@@ -101,9 +97,9 @@ class Internal_loop(Module):
         ) 
         
     @module.combinational
-    def build(self, outter_loop: External_loop ,sram_master_flag:Array):
+    def build(self, outter_loop: External_loop ,sram_master_flag:Array,j:Array):
         
-        j = RegArray(Int(32), 1)
+        
         con = Bits(1)(0)
         full_flag = Bits(1)(0)
         full_flag = (j[0] == (Int(32)(J_MAX))) & sram_master_flag[0]
@@ -111,9 +107,9 @@ class Internal_loop(Module):
         with Condition(sram_master_flag[0] == Bits(1)(1)):
             con = j[0] < Int(32)(J_MAX)
             j[0] = con.select((j[0].bitcast(Int(32)) + Int(32)(1)) , Int(32)(0))
-        
+        log("innerloop----------sram_master_flag: {} | j: {} |full_flag: {}", sram_master_flag[0],j[0],full_flag)
         outter_loop.async_called( In_full_flag = full_flag)
-        return j
+        
 
 
 
@@ -125,27 +121,28 @@ class Driver(Module):
         )
  
     @module.combinational
-    def build(self, inner_loop: Internal_loop ,user: Loop_user):
+    def build(self, inner_loop: Internal_loop ):
         inner_loop.async_called()
-        user.async_called()
+        
 
 def test_spmv():
     sys =  SysBuilder('spmv')
     with sys:
+        i = RegArray(Int(32), 1)
+        j = RegArray(Int(32), 1)
+
         sram_master = SRAM_Master()
-        sram_master_flag = sram_master.build()
+        sram_master_flag = sram_master.build(i,j)
         
         external_loop = External_loop()
-        i = external_loop.build(sram_master)
+        external_loop.build(sram_master,i)
 
         internal_loop = Internal_loop()
-        j = internal_loop.build(external_loop,sram_master_flag)
-
-        loop_user = Loop_user()
-        loop_user.build(i, j)
+        internal_loop.build(external_loop,sram_master_flag,j)
 
         driver = Driver()
-        driver.build(internal_loop, loop_user)
+        driver.build(internal_loop)
+
     conf = config(
         verilog=utils.has_verilator(),
         sim_threshold=100,
