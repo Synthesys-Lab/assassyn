79670417fdef7bf8a1a3c632baf959dccde6544e
lychee <lychee@lycheedeMacBook-Air.local>
2024-11-27 22:10:51 +0800

    modify mod and decode

--- examples/o3-cpu/src/memory_access.py
+++ examples/o3-cpu/src/memory_access.py
@@ -1,42 +1,50 @@
  
 from assassyn.frontend import *
 from opcodes import *
-from scoreboard import *
-
 class MemoryAccess(Module):
     
     def __init__(self):
         super().__init__(
-            ports={'rdata': Port(Bits(32)), 'rd': Port(Bits(5))},
+            ports={'rdata': Port(Bits(32)), 'rd': Port(Bits(5)),'mem_ext' : Port(Bits(2)),'result': Port(Bits(32))},
             no_arbiter=True)
         self.name = 'm'
+
     @module.combinational
     def build(
         self, 
-        writeback: Module,
-        scoreboard:Array,
-        RMT:Array
+        writeback: Module, 
+        mem_bypass_reg: Array, 
+        mem_bypass_data: Array,
+        wb_bypass_data: Array,
+        wb_bypass_reg: Array
     ):
         self.timing = 'systolic'
 
+        data_cut = Bits(32)(0)
+        mem_ext = self.mem_ext.pop()
+        result = self.result.pop()
+        rd = self.rd.pop()
         with Condition(self.rdata.valid()):
             data = self.rdata.pop()
-            rd = self.rd.pop()
             log("mem.rdata        | 0x{:x}", data)
+            mem_bypass_reg[0] = rd
             with Condition(rd != Bits(5)(0)):
                 log("mem.bypass       | x{:02} = 0x{:x}", rd, data)
-                for index in range(SCOREBOARD.size):
-                    Idx_mem=UInt(5)(index)
-                    with Condition(scoreboard[index].sb_valid & ~scoreboard[index].sb_status):
-                        log(" rs1_ready {:03x}  rs2_ready {:03x}  rs1_dep {:06} rs2_dep {:06} rmt[rd] {:06} ",\
-                            scoreboard[index].rs1_ready,scoreboard[index].rs2_ready ,  scoreboard[index].rs1_dep ,\
-                                scoreboard[index].rs2_dep ,RMT[rd]  )
-                        rs1_get_data = ~scoreboard[index].rs1_ready & (scoreboard[index].rs1_dep== RMT[rd]) 
-                        rs2_get_data = ~scoreboard[index].rs2_ready & (scoreboard[index].rs2_dep== RMT[rd] )
-                        with Condition (rs1_get_data | rs2_get_data ):
-                            scoreboard[index] =modify_entry_rs(scoreboard,index,rs1_get_data,rs2_get_data,data)
-    
-                RMT[rd]=Bits(SCOREBOARD.Bit_size)(SCOREBOARD.size)
+                mem_bypass_data[0] = data
+
+        with Condition(~self.rdata.valid()):
+            mem_bypass_reg[0] = Bits(5)(0)
 
         arg = self.rdata.valid().select(self.rdata.peek(), Bits(32)(0))
-        writeback.async_called(mdata = arg)
+        sign = arg[7:7]
+        ext = sign.select(Bits(24)(0xffffff), Bits(24)(0))
+        data_cut = mem_ext[1:1].select( Bits(24)(0).concat(arg[0:7]) , ext.concat(arg[0:7]) )
+        
+        arg = self.rdata.valid().select(arg, result)
+        arg = mem_ext[0:0].select( data_cut ,arg)
+
+        wb_bypass_data[0] = arg
+        wb_bypass_reg[0] = rd
+
+        wb_bound = writeback.bind(mdata = arg , rd = rd)
+        wb_bound.async_called() 
--- examples/o3-cpu/src/writeback.py
+++ examples/o3-cpu/src/writeback.py
@@ -1,60 +1,22 @@
 from assassyn.frontend import *
-from opcodes import * 
-from scoreboard import *
+from opcodes import *
+
 class WriteBack(Module):
     
     def __init__(self):
         super().__init__(
             ports={
-                'is_memory_read': Port(Bits(1)),
-                'result': Port(Bits(32)),
                 'rd': Port(Bits(5)),
                 'mdata': Port(Bits(32)),
-                'is_csr': Port(Bits(1)),
-                'csr_id': Port(Bits(4)),
-                'csr_new': Port(Bits(32)),
-                'mem_ext': Port(Bits(2)),
-                'index':Port(Bits(SCOREBOARD.Bit_size))
             }, no_arbiter=True)
 
         self.name = 'W'
 
     @module.combinational
-    def build(self, reg_file: Array , csr_file: Array,scoreboard:Array,RMT:Array,sb_head:Array):
+    def build(self, reg_file: Array ):
 
-        is_memory_read, result, rd, mdata , is_csr , csr_id , csr_new , mem_ext,index= self.pop_all_ports(True)
-        wb_valid =( sb_head[0]==index)
-        log("sb_head {:07}  index {:07} ",sb_head[0], index)
-        wait_until(wb_valid)
-        with Condition(wb_valid):
-            log("Wb_valid")
-            data_cut = Bits(32)(0)
-            sign = mdata[7:7]
-            ext = sign.select(Bits(24)(0xffffff), Bits(24)(0))
-            data_cut = mem_ext[1:1].select( Bits(24)(0).concat(mdata[0:7]) , ext.concat(mdata[0:7]) )
-
-            data = is_memory_read.select(mdata, result)
-            data = mem_ext[0:0].select( data_cut ,data)
-
-            with Condition((rd != Bits(5)(0))):
-                log("writeback        | x{:02}          | 0x{:08x}", rd, data)
-                reg_file[rd] = data
-
-                
-                
-
-
-            with Condition(is_csr):
-                log("writeback        | csr[{:02}]       | 0x{:08x}", csr_id, csr_new)
-                csr_file[csr_id] = csr_new
-
-             
-        
-
-            scoreboard[index] = modify_entry_valid(scoreboard,index,Bits(1)(0))
-            sb_head[0]= (((sb_head[0].bitcast(UInt(32)) )+UInt(32)(1) )%SCOREBOARD.sizeI ).bitcast(Bits(SCOREBOARD.Bit_size))
-            
-            log("Update sb_head {:07} ",sb_head[0])
-            log("out writeback")
-            #Todo:make it commit in order  
-            return rd
+        rd, mdata = self.pop_all_ports(False)
+        with Condition((rd != Bits(5)(0))):
+            log("writeback        | x{:02}          | 0x{:08x}", rd, mdata)
+            reg_file[rd] = mdata
+        return rd
