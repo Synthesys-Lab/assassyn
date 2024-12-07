4b3bbc5e58b4f2316dcb9d3fd51bdac2e41798b3
WanningZhang <nkzwning@outlook.com>
2024-12-07 16:26:22 +0800

    Create a minimal test case.

---
+++ examples/proto/main.py
@@ -0,0 +1,34 @@
+import pytest
+
+import assassyn
+from assassyn.frontend import *
+from assassyn.backend import elaborate
+from assassyn import utils
+
+class Testbench(Module):    
+    def __init__(self):
+        super().__init__(ports={})
+
+    @module.combinational
+    def build(self):
+        with Cycle(1):
+            log("OK")
+
+def test_proto():
+    sys = SysBuilder('minimal')
+    with sys:        
+        testbench = Testbench()
+        testbench.build()
+
+    config = assassyn.backend.config(sim_threshold=20, idle_threshold=20, verilog=utils.has_verilator())
+    simulator_path, verilator_path = elaborate(sys, **config)
+
+    raw = utils.run_simulator(simulator_path)
+    print(raw)
+    
+    if verilator_path:
+        raw = utils.run_verilator(verilator_path)
+
+
+if __name__ == '__main__':
+    test_proto()
