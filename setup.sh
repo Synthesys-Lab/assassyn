22c1dfcea00654ac6c96158207b5a326c77009ab
WanningZhang <nkzwning@outlook.com>
2025-01-26 20:22:34 +0300

    Move `_pb2.py` to build flow.

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 # NOTE: This script should be sourced by ZSH! O.w. the directory behaviors will be wrong!
 
-# Restore the original directory
+# Restore the original directory 
 RESTORE_DIR=`pwd`
 
 # Go to the setup.sh directory
@@ -9,14 +9,13 @@ cd `dirname $0`
 # Use the repository path to set the PYTHONPATH and ASSASSYN_HOME
 REPO_PATH=`git rev-parse --show-toplevel`
 
-
 which sccache > /dev/null 2>&1
 
 if [ $? -eq 0 ]; then
-  echo "Setting up SCCACHE to $SCCACHE"
-  export RUSTC_WRAPPER=`which sccache`
+ echo "Setting up SCCACHE to $SCCACHE"
+ export RUSTC_WRAPPER=`which sccache`
 else
-  echo "No sccache found! Skip!"
+ echo "No sccache found! Skip!"
 fi
 
 echo "Adding $REPO_PATH/python to PYTHONPATH"
@@ -26,9 +25,13 @@ echo "Setting ASSASSYN_HOME to $REPO_PATH"
 export ASSASSYN_HOME=$REPO_PATH
 
 if [ -d $REPO_PATH/verilator ]; then
-  echo "In-repo verilator found, setting VERILATOR_ROOT to $REPO_PATH/verilator"
-  export VERILATOR_ROOT=$REPO_PATH/verilator
+ echo "In-repo verilator found, setting VERILATOR_ROOT to $REPO_PATH/verilator"
+ export VERILATOR_ROOT=$REPO_PATH/verilator
 fi
+
+echo "Generating protobuf files..."
+rm -f $REPO_PATH/python/assassyn/create_pb2.py
+protoc --python_out=$REPO_PATH/python/assassyn --proto_path=$REPO_PATH/python/assassyn $REPO_PATH/python/assassyn/create.proto
 
 # Go back to the original directory
 cd $RESTORE_DIR
