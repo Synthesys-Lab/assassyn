1fc98b5693a4ba8f0018170056d2cc92b4eca9e5
lyzx2001 <rep_laureline@hotmail.com>
2025-02-19 21:45:02 +0300

    fix execution bug

--- examples/fft/fft.py
+++ examples/fft/fft.py
@@ -14,19 +14,19 @@ from assassyn import utils
 I_MAX = 5
 FFT_SIZE = 16 # 最终1024
 
-ADDR_WIDTH = 11
+ADDR_WIDTH = 12
 
 class SRAM_USER:
-    S0 = Int(32)(0)
-    S1 = Int(32)(1)
-    S2 = Int(32)(2)
-    S3 = Int(32)(3)
-    S4 = Int(32)(4)
-    S5 = Int(32)(5)
-    S6 = Int(32)(6)
-    S7 = Int(32)(7)
-    S8 = Int(32)(8)
-    S9 = Int(32)(9)
+    S0 = Bits(4)(0)
+    S1 = Bits(4)(1)
+    S2 = Bits(4)(2)
+    S3 = Bits(4)(3)
+    S4 = Bits(4)(4)
+    S5 = Bits(4)(5)
+    S6 = Bits(4)(6)
+    S7 = Bits(4)(7)
+    S8 = Bits(4)(8)
+    S9 = Bits(4)(9)
 
 class Loop_user(Module):
 
@@ -51,20 +51,20 @@ class Calculate_loop(Module):
         )
 
     @module.combinational
-    def build(self, rootindex: Array):
+    def build(self, state: Array, rootindex: Array):
         In_full_flag = self.pop_all_ports(True)
-        state = RegArray(Int(32), 1)
+        # state = RegArray(Int(32), 1)
         con = Bits(1)(0)
-        con = state[0] < Int(32)(9)
-        full_flag = state[0] == Int(32)(I_MAX)
+        con = state[0] < SRAM_USER.S9
+        # full_flag = state[0] == Int(32)(I_MAX)
         
-        state_value = Int(32)(0)
-        state_value = con.select((state[0].bitcast(Int(32)) + Int(32)(1)) , Int(32)(0))
+        state_value = Bits(4)(0)
+        state_value = con.select((state[0].bitcast(Int(4)) + Int(4)(1)).bitcast(Bits(4)) , Bits(4)(0))
         
         con = rootindex[0].bitcast(Int(32)) == Int(32)(0)
-        state_value = con.select(Int(32)(9), state_value)
+        state_value = con.select(SRAM_USER.S9, state_value)
         
-        state[0] = state_value.bitcast(Int(32))
+        state[0] = state_value.bitcast(Bits(4))
         
         # state[0] = con.select((state[0].bitcast(Int(32)) + Int(32)(1)) , Int(32)(0)) #xxx
         
@@ -107,18 +107,18 @@ class External_loop(Module):
         
         odd = RegArray(Int(32), 1, initializer=[0])
         span = RegArray(Int(32), 1, initializer=[FFT_SIZE >> 1])
-        log = RegArray(Int(32), 1, initializer=[0])
+        log0 = RegArray(Int(32), 1, initializer=[0])
         even = RegArray(Int(32), 1, initializer=[0])
         temp1 = RegArray(Int(32), 1, initializer=[0])
         temp2 = RegArray(Int(32), 1, initializer=[0])
         temp3 = RegArray(Int(32), 1, initializer=[0])
         temp4 = RegArray(Int(32), 1, initializer=[0])
-        out1 = RegArray(Int(32), 1, initializer=[0])
-        out2 = RegArray(Int(32), 1, initializer=[0])
+        out1 = RegArray(Int(64), 1, initializer=[0])
+        out2 = RegArray(Int(64), 1, initializer=[0])
         # rootindex = RegArray(Int(32), 1, initializer=[0])
         temp5 = RegArray(Int(32), 1, initializer=[0])
         temp6 = RegArray(Int(32), 1, initializer=[0])
-        out3 = RegArray(Int(32), 1, initializer=[0])
+        out3 = RegArray(Int(64), 1, initializer=[0])
         
         re = Bits(1)(0)
         we = Bits(1)(0)
@@ -126,8 +126,6 @@ class External_loop(Module):
         we = (state[0] == SRAM_USER.S4)|(state[0] == SRAM_USER.S5)|(state[0] == SRAM_USER.S9)
         
         address_wire = Bits(ADDR_WIDTH)(0)
-        
-        log("xxx")
         
         # with Condition(state[0] == SRAM_USER.S9):
         #     with Condition(span[0] == Int(32)(0)):
@@ -152,8 +150,8 @@ class External_loop(Module):
             temp2[0] = even_reg[0][32:63].bitcast(Int(32)) - odd_reg[0][32:63].bitcast(Int(32))
             temp3[0] = even_reg[0][0:31].bitcast(Int(32)) + odd_reg[0][0:31].bitcast(Int(32))
             temp4[0] = even_reg[0][0:31].bitcast(Int(32)) - odd_reg[0][0:31].bitcast(Int(32))
-            out1[0] = concat(temp1[0].bitcast(Int(32)), temp3[0].bitcast(Int(32))).bitcast(Int(32)) # even
-            out2[0] = concat(temp2[0].bitcast(Int(32)), temp4[0].bitcast(Int(32))).bitcast(Int(32)) # odd
+            out1[0] = concat(temp1[0].bitcast(Int(32)), temp3[0].bitcast(Int(32))).bitcast(Int(64)) # even
+            out2[0] = concat(temp2[0].bitcast(Int(32)), temp4[0].bitcast(Int(32))).bitcast(Int(64)) # odd
         with Condition(state[0] == SRAM_USER.S4):
             # address_wire = even[0][0:ADDR_WIDTH-1].bitcast(Bits(ADDR_WIDTH))
             out[0] = out1[0].bitcast(Int(64))
@@ -162,15 +160,15 @@ class External_loop(Module):
             out[0] = out2[0].bitcast(Int(64))
         
         with Condition(state[0] == SRAM_USER.S6):
-            rootindex[0] = ((even[0].bitcast(Int(32)) << log[0].bitcast(Int(32))) & (Int(32)(FFT_SIZE - 1))).bitcast(Int(32))
+            rootindex[0] = ((even[0].bitcast(Int(32)) << log0[0].bitcast(Int(32))) & (Int(32)(FFT_SIZE - 1))).bitcast(Int(32))
             # with Condition(rootindex[0] == Int(32)(0)):
             #     state[0] = SRAM_USER.S8.bitcast(Int(32))
         # with Condition(state[0] == SRAM_USER.S7):
         #     address_wire = (rootindex[0][0:ADDR_WIDTH-1].bitcast(Int(32)) + Int(32)(1024)).bitcast(Bits(ADDR_WIDTH))
         with Condition(state[0] == SRAM_USER.S8):
-            temp5[0] = (twid_reg[0][32:63].bitcast(Int(32)) * odd_reg[0][32:63].bitcast(Int(32)) - twid_reg[0][0:31].bitcast(Int(32)) * odd_reg[0][0:31].bitcast(Int(32))).bitcast(Int(32))
-            temp6[0] = (twid_reg[0][32:63].bitcast(Int(32)) * odd_reg[0][0:31].bitcast(Int(32)) - twid_reg[0][0:31].bitcast(Int(32)) * odd_reg[0][32:63].bitcast(Int(32))).bitcast(Int(32))
-            out3[0] = concat(temp5[0].bitcast(Int(32)), temp6[0].bitcast(Int(32))).bitcast(Int(32))
+            temp5[0] = (twid_reg[0][32:63].bitcast(Int(32)) * odd_reg[0][32:63].bitcast(Int(32)) - twid_reg[0][0:31].bitcast(Int(32)) * odd_reg[0][0:31].bitcast(Int(32)))[0:31].bitcast(Int(32))
+            temp6[0] = (twid_reg[0][32:63].bitcast(Int(32)) * odd_reg[0][0:31].bitcast(Int(32)) - twid_reg[0][0:31].bitcast(Int(32)) * odd_reg[0][32:63].bitcast(Int(32)))[0:31].bitcast(Int(32))
+            out3[0] = concat(temp5[0].bitcast(Int(32)), temp6[0].bitcast(Int(32))).bitcast(Int(64))
         with Condition(state[0] == SRAM_USER.S9):
             with Condition(rootindex[0] != Int(32)(0)):
                 # address_wire = odd[0][0:ADDR_WIDTH-1].bitcast(Bits(ADDR_WIDTH))
@@ -183,16 +181,15 @@ class External_loop(Module):
             con = odd[0] == Int(32)(FFT_SIZE)
             odd[0] = con.select(Int(32)(0), (odd[0].bitcast(Int(32)) + Int(32)(1)))
             span[0] = con.select((span[0].bitcast(Int(32)) >> Int(32)(1)), span[0])
-            log[0] = con.select((log[0].bitcast(Int(32)) + Int(32)(1)), log[0])
+            log0[0] = con.select((log0[0].bitcast(Int(32)) + Int(32)(1)), log0[0])
         
-        log("yyy")
         
         address_wire = state[0].case({
             SRAM_USER.S1: even[0][0:ADDR_WIDTH-1].bitcast(Bits(ADDR_WIDTH)),
             SRAM_USER.S2: odd[0][0:ADDR_WIDTH-1].bitcast(Bits(ADDR_WIDTH)),
             SRAM_USER.S4: even[0][0:ADDR_WIDTH-1].bitcast(Bits(ADDR_WIDTH)),
             SRAM_USER.S5: odd[0][0:ADDR_WIDTH-1].bitcast(Bits(ADDR_WIDTH)),
-            SRAM_USER.S7: (rootindex[0][0:ADDR_WIDTH-1].bitcast(Int(32)) + Int(32)(1024)).bitcast(Bits(ADDR_WIDTH)),
+            SRAM_USER.S7: (rootindex[0][0:ADDR_WIDTH-1].bitcast(Int(ADDR_WIDTH)) + Int(ADDR_WIDTH)(1024)).bitcast(Bits(ADDR_WIDTH)),
             SRAM_USER.S9: odd[0][0:ADDR_WIDTH-1].bitcast(Bits(ADDR_WIDTH)),
             None: Bits(ADDR_WIDTH)(0)
         })
@@ -200,8 +197,6 @@ class External_loop(Module):
         sram.build(we, re, address_wire, out[0].bitcast(Bits(64)), memuser)
         with Condition(state[0] == SRAM_USER.S1 | state[0] == SRAM_USER.S2 | state[0] == SRAM_USER.S4 | state[0] == SRAM_USER.S5 | state[0] == SRAM_USER.S7 | state[0] == SRAM_USER.S9):
             sram.bound.async_called()
-        
-        # 'Array' object is not callable?
         # log("address_wire: {} even_reg: {} | odd_reg: {}", address_wire, even_reg[0], odd_reg[0])
         # log("we: {} | re: {} | out: {}", we, re, out[0].bitcast(Bits(64)))
         
@@ -210,7 +205,6 @@ class External_loop(Module):
         
         calculate_loop.async_called( In_full_flag = full_flag.bitcast(Bits(1)))
         
-        log("zzz")
         return span, odd
 
 
@@ -234,10 +228,11 @@ def test_fft():
         odd_reg = RegArray(Int(64), 1)
         twid_reg = RegArray(Int(64), 1)
         
+        state = RegArray(Bits(4), 1)
         rootindex = RegArray(Int(32), 1, initializer=[0])
         
         calculate_loop = Calculate_loop()
-        state = calculate_loop.build(rootindex)
+        calculate_loop.build(state, rootindex)
 
         out = RegArray(Int(64), 1)
 
@@ -262,8 +257,8 @@ def test_fft():
 
     raw = utils.run_simulator(simulator_path)
     
-    # if verilator_path:
-    #     raw = utils.run_verilator(verilator_path)
+    if verilator_path:
+        raw = utils.run_verilator(verilator_path)
 
 if __name__ == '__main__':
     test_fft()
