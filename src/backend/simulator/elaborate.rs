bf67d738546dcb9c45b7a412824f20827070b6aa
Jian Weng <jian.weng465@gmail.com>
2025-05-10 17:55:11 -0700

    fix clippy

--- src/backend/simulator/elaborate.rs
+++ src/backend/simulator/elaborate.rs
@@ -11,13 +11,12 @@ use quote::quote;
 
 use crate::{
   analysis::{
-    find_critical_path::{DependencyGraph, GraphVisitor},
+    find_critical_path::GraphVisitor,
     topo_sort,
   },
   backend::common::{create_and_clean_dir, upstreams, Config},
   builder::system::{ModuleKind, SysBuilder},
   ir::{expr::subcode, instructions::PureIntrinsic, node::*, visitor::Visitor, *},
-  xform::barrier_analysis::GatherModulesToCut,
 };
 
 use super::utils::{dtype_to_rust_type, namify};
