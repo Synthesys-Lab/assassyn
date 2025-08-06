46bf13df8e3b4c2eeb43bfb706f4f5f3d9c9256a
Jian Weng <jian.weng465@gmail.com>
2025-08-06 17:19:59 +0800

    ok, i hope this cleanup works fine

--- init.sh
+++ init.sh
@@ -1,2 +1,9 @@
 #!/usr/bin/env zsh
 git submodule update --init
+
+source ./scripts/init/py-package.sh
+source ./scripts/init/circt.sh
+source ./scripts/init/verilator.sh
+
+source ./scripts/init/ramulator2.sh
+source ./scripts/init/wrapper.sh
