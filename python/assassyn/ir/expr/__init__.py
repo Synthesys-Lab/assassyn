ddb3b88fa2adb39d9b5e40fab9fe0a58468ba2c5
Jian Weng <jian.weng465@gmail.com>
2025-10-04 18:51:33 +0300

    [refactor] Move BinaryOp and UnaryOp to arith module.
    [python/assassyn/ir/expr/arith.py] Add new module with BinaryOp and UnaryOp classes.
    [python/assassyn/ir/expr/expr.py] Remove BinaryOp and UnaryOp classes.
    [python/assassyn/ir/expr/__init__.py] Export classes from arith module.

--- python/assassyn/ir/expr/__init__.py
+++ python/assassyn/ir/expr/__init__.py
@@ -2,6 +2,7 @@
 
 #pylint: disable=wildcard-import
 from .expr import *
+from .arith import *
 from .intrinsic import Intrinsic, PureIntrinsic, finish, wait_until, assume, barrier
 from .intrinsic import mem_write, send_read_request, send_write_request
 from .intrinsic import has_mem_resp, mem_resp, use_dram
