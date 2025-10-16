558ead763fbfcc6003c555dc161f58a7107c9ddd
Jian Weng <jian.weng465@gmail.com>
2025-10-16 14:43:17 +0300

    [feature] Apply @enforce_type to ir/expr/array.py
    [python/assassyn/ir/expr/array.py] Add type enforcement to ArrayWrite and ArrayRead
    [python/assassyn/utils/__init__.py] Fix import compatibility
    Addresses Phase 2 type inconsistencies with runtime validation.

--- python/assassyn/ir/expr/array.py
+++ python/assassyn/ir/expr/array.py
@@ -8,6 +8,7 @@ import typing
 
 from ..value import Value
 from .expr import Expr
+from ...type_utils.enforce_type import enforce_type
 
 if typing.TYPE_CHECKING:
     from ..array import Array
@@ -20,7 +21,8 @@ class ArrayWrite(Expr):
 
     ARRAY_WRITE = 401
 
-    def __init__(self, arr, idx: Value, val: Value, module: ModuleBase = None):
+    @enforce_type
+    def __init__(self, arr, idx: Value, val: Value, module: 'ModuleBase' = None):
         super().__init__(ArrayWrite.ARRAY_WRITE, [arr, idx, val])
         # Get module from Singleton if not provided
         if module is None:
@@ -57,7 +59,8 @@ class ArrayRead(Expr):
 
     ARRAY_READ = 400
 
-    def __init__(self, arr: Array, idx: Value):
+    @enforce_type
+    def __init__(self, arr: 'Array', idx: Value):
         # pylint: disable=import-outside-toplevel
         from ..array import Array
         assert isinstance(arr, Array), f'{type(arr)} is not an Array!'
--- python/assassyn/utils/__init__.py
+++ python/assassyn/utils/__init__.py
@@ -3,12 +3,36 @@
 # Import new type enforcement utilities
 from .enforce_type import enforce_type, validate_arguments, check_type
 
-# Import existing utilities from the main utils module
-# Note: We avoid importing from ..utils to prevent circular imports
-# Instead, users should import directly from assassyn.utils for existing functions
-# and from assassyn.utils.enforce_type for type enforcement
+# Import existing utilities from the main utils.py file
+# We use a different approach to avoid circular imports
+import importlib.util
+import os
+
+# Load the main utils.py file
+utils_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'utils.py')
+spec = importlib.util.spec_from_file_location("utils", utils_path)
+utils_module = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(utils_module)
+
+# Export all the functions
+identifierize = utils_module.identifierize
+unwrap_operand = utils_module.unwrap_operand
+repo_path = utils_module.repo_path
+package_path = utils_module.package_path
+patch_fifo = utils_module.patch_fifo
+run_simulator = utils_module.run_simulator
+run_verilator = utils_module.run_verilator
+parse_verilator_cycle = utils_module.parse_verilator_cycle
+parse_simulator_cycle = utils_module.parse_simulator_cycle
+has_verilator = utils_module.has_verilator
+create_dir = utils_module.create_dir
+namify = utils_module.namify
 
 __all__ = [
+    # Existing utilities
+    'identifierize', 'unwrap_operand', 'repo_path', 'package_path',
+    'patch_fifo', 'run_simulator', 'run_verilator', 'parse_verilator_cycle',
+    'parse_simulator_cycle', 'has_verilator', 'create_dir', 'namify',
     # New type enforcement utilities
     'enforce_type', 'validate_arguments', 'check_type'
 ]
